e235d31214cb2e7af5bf611bc1e53bac563d7ed6
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-08-10 21:30:13 +0700

    fix: import

--- src/app/profile/profileRoute.ts
+++ src/app/profile/profileRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { VerifyToken } from "middleware/verifyToken";
-import { CatchWrapper } from "utils/CatchWrapper";
+import { VerifyToken } from "../../middleware/verifyToken";
+import { CatchWrapper } from "../../utils/CatchWrapper";
 import { getProfileController } from "./profileController";
 
 const route = Router()
