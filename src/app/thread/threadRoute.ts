15481f4c8b3328a6c8c00ee9a1fb8341c10a9e7e
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-08-10 21:32:21 +0700

    fix: import

--- src/app/thread/threadRoute.ts
+++ src/app/thread/threadRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { VerifyToken } from "middleware/verifyToken";
-import { CatchWrapper } from "utils/CatchWrapper";
+import { VerifyToken } from "../../middleware/verifyToken";
+import { CatchWrapper } from "../../utils/CatchWrapper";
 import { createThreadController } from "./threadController";
 
 export const route = Router()
